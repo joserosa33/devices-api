f1514692bd17d551aad7815333ab581e66effc51
jose.rosa <jose.rosa@farfetch.com>
2021-10-31 18:28:07 +0000

    feat: Update routes

--- src/routers/devicesRouter.go
+++ src/routers/devicesRouter.go
@@ -9,12 +9,12 @@ import (
 func GetDeviceRouter(deviceController controllers.DeviceController) *mux.Router {
 	router := mux.NewRouter()
 
-	router.HandleFunc("/device", deviceController.All).Methods("GET")
-	router.HandleFunc("/device", deviceController.Add).Methods("POST")
-	router.HandleFunc("/device/{id}", deviceController.Get).Methods("GET")
-	router.HandleFunc("/device", deviceController.Update).Methods("PUT")
-	router.HandleFunc("/device/{id}", deviceController.Delete).Methods("DELETE")
-	router.HandleFunc("/device/brand/{brand}", deviceController.GetByBrand).Methods("GET")
+	router.HandleFunc("/devices", deviceController.All).Methods("GET")
+	router.HandleFunc("/devices", deviceController.Add).Methods("POST")
+	router.HandleFunc("/devices/{id}", deviceController.Get).Methods("GET")
+	router.HandleFunc("/devices", deviceController.Update).Methods("PUT")
+	router.HandleFunc("/devices/{id}", deviceController.Delete).Methods("DELETE")
+	router.HandleFunc("/devices/brands/{brand}", deviceController.GetByBrand).Methods("GET")
 
 	return router
 }
